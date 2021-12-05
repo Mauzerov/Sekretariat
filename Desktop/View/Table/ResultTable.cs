c6ee7e9e744bfb75b10fe52508a4ae9b68f50078
Mateusz Mazurek <mauzer.official.1@gmail.com>
2021-12-05 20:59:30 +0100

    Move FQL Updated ResultTable.cs TODO: Sortable Array By Given Integer Index - Get Index From SortableHeader - SortableButton Gets Index Whilst Being Initialized As SortableHeader Element TODO: Where Generator

--- Desktop/View/Table/ResultTable.cs
+++ Desktop/View/Table/ResultTable.cs
@@ -10,13 +10,22 @@ namespace Desktop.View.Table
 {
     public class ResultTable : Grid
     {
+        public IEnumerable<IEnumerable<IComparable>> Result;
+        public IEnumerable<string> Fields;
+        
         public ResultTable(IEnumerable<string> fields, IEnumerable<IEnumerable<IComparable>> data)
+        {
+            Fields = fields;
+            Result = data;
+        }
+
+        public void Generate()
         {
             Children.Clear();
             var column = 0;
             var header = new SortableHeader();
             RowDefinitions.Add(new RowDefinition());
-            foreach (var r in fields)
+            foreach (var r in Fields)
             {
                 ColumnDefinitions.Add(new ColumnDefinition());
                 var label = new SortableButton(header)
@@ -28,7 +37,7 @@ namespace Desktop.View.Table
             }
 
             var index = 0;
-            foreach (var row in data)
+            foreach (var row in Result)
             {
                 RowDefinitions.Add(new RowDefinition());
                 column = 0;
